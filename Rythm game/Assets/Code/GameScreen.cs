32418ebe700d4b2e0da2385815adc270aeb3c4d6
M3-T4L <lorinczmatee@gmail.com>
2025-02-24 18:50:41 +0100

    arrows + randomizer

--- Rythm game/Assets/Code/GameScreen.cs
+++ Rythm game/Assets/Code/GameScreen.cs
@@ -3,13 +3,24 @@ using UnityEngine.SceneManagement;
 
 public class GameScreen : MonoBehaviour
 {
+
+    ///Array for sprite display
+
+    [SerializeField] Sprite [] arrowImages;
+
+
+    [SerializeField] SpriteRenderer spriteRenderer;
+    int randomArrow;
+
     public void XButton()
     {
         SceneManager.LoadSceneAsync("menu");
     }
 
-    public void StartGameButton()
+    public void ArrowRandomizer()
     {
-
+        randomArrow = Random.Range(1, arrowImages.Length + 1);
+        spriteRenderer.sprite = arrowImages[randomArrow - 1];
+        print(randomArrow);
     }
 }
