0b9852e2054850818e5585bf04a084e8ddab9527
M3-T4L <lorinczmatee@gmail.com>
2025-03-23 20:46:19 +0100

    menu update + game screen update + song midi

--- Rythm game/Assets/Code/MainMenuu.cs
+++ Rythm game/Assets/Code/MainMenuu.cs
@@ -11,6 +11,11 @@ public class MainMenuu : MonoBehaviour
 
     }
 
+    public void Profile()
+    {
+        SceneManager.LoadSceneAsync("profile 2");
+    }
+
     public void Settings()
     {
         SceneManager.LoadSceneAsync("SettingsScreen");
